f3ae056699b07e29893db25c4778d339ad9fa251
hefangqiang <971603793@qq.com>
2018-11-22 10:59:12 +0800

    整体提交项目初始版本

---
+++ mavenModule1/src/test/TestLog.java
@@ -0,0 +1,44 @@
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.naming.Name;
+
+
+/**
+ * @description： TODO
+ * @author： Mr.He
+ * @date： 2018-11-09 13:33
+ **/
+public class TestLog {
+
+
+
+    @Test
+    public void log1() {
+        Logger log= LoggerFactory.getLogger(TestLog.class);
+        String name="测试1";
+        log.info("这是一1条日志{ }",name);
+
+    }
+
+    @Test
+    public void log2() {
+        Logger log= LoggerFactory.getLogger("paperLog");
+        String name="测试2";
+        log.info("这是一条日志{ }", name );
+    }
+
+    @Test
+    public void log3() {
+
+    }
+
+    @Test
+    public void log4() {
+
+    }
+
+
+}
